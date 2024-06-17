9f359a42283dd3bef4757cae87565404493e8247
Alexander Larsen <alexander.larsen@keysight.com>
2024-06-17 13:29:59 +0200

    Rename ephemeral (#20)
    This renames the mysterious Ephemeral bool to push / poll, which should
    clear up confusion about the intended use. A poll metric will be polled,
    and a push metric can be pushed. This also leaves us the opportunity to
    create new types of metrics in the future, though I can't currently see
    what other semantics would make sense.

--- OpenTap.Metrics/MetricKind.cs
+++ OpenTap.Metrics/MetricKind.cs
@@ -1,9 +1,14 @@
+using System;
+
 namespace OpenTap.Metrics;
 
+[Flags]
 public enum MetricKind
 {
-    Unknown,
-    Double,
-    Boolean,
-    String
+    /// <summary> This metric can be polled. </summary>
+    Poll = 1,
+    /// <summary> This metric can be pushed out of band. </summary>
+    Push = 2,
+    /// <summary> This metric can be polled and pushed out of band. </summary>
+    PushPoll = Push | Poll,
 }
