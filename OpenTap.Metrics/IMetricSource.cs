b97e81789e7338d8d21a1f307bbc397f2c02285f
Alexander Larsen <alexander.larsen@keysight.com>
2024-06-25 13:39:13 +0200

    29 update package xml (#33)
    * Add source url, owner, and license. Update description
    
    * Disable obfuscation
    
    * Add copyright notice to all source files
    
    * Add license file

--- OpenTap.Metrics/IMetricSource.cs
+++ OpenTap.Metrics/IMetricSource.cs
@@ -1,3 +1,7 @@
+//            Copyright Keysight Technologies 2012-2024
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
 namespace OpenTap.Metrics;
 
 /// <summary> Marker interface for classes that produce metrics. </summary>
--- OpenTap.Metrics/IOnPollMetricsCallback.cs
+++ OpenTap.Metrics/IOnPollMetricsCallback.cs
@@ -1,3 +1,7 @@
+//            Copyright Keysight Technologies 2012-2024
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
 using System.Collections.Generic;
 
 namespace OpenTap.Metrics;
