e6cc3e55628cf30842331dca34b51c96084765d8
Alexander Larsen <alexander.larsen@keysight.com>
2024-06-28 17:32:57 +0200

    Update XML docs

--- OpenTap.Metrics/IMetricListener.cs
+++ OpenTap.Metrics/IMetricListener.cs
@@ -2,13 +2,12 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, you can obtain one at http://mozilla.org/MPL/2.0/.
-using System.Collections.Generic;
 
 namespace OpenTap.Metrics;
 
-/// <summary> Defines that a class can consume metrics. </summary>
+/// <summary> Indicates that a class can consume metrics. </summary>
 public interface IMetricListener
 {
-    /// <summary>  Event occuring when a metric producer generates out-of-band metrics. </summary>
+    /// <summary>  Event occuring when a metric is pushed. </summary>
     void OnPushMetric(IMetric table);
 }
