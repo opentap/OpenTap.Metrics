e6a6ab0c1168cb557490bb3d83a6eae896958ef7
Asger Iversen <20689829+AsgerIversen@users.noreply.github.com>
2024-12-06 11:54:23 +0100

    Renamed ResourceDiscovery to AssetDiscovery

--- OpenTap.Metrics/ResourceDiscovery/IResourceDiscovery.cs
+++ OpenTap.Metrics/ResourceDiscovery/IResourceDiscovery.cs
@@ -1,41 +1,42 @@
 using System.Collections.Generic;
 
-namespace OpenTap.Metrics.ResourceDiscovery;
+namespace OpenTap.Metrics.AssetDiscovery;
 
 /// <summary>
-/// Plugin interface for resource discovery.
-/// Implementations of this interface can be used to discover resources such as instruments or DUTs connected to the system.
+/// Plugin interface for asset discovery.
+/// Implementations of this interface can be used to discover assets such as instruments or DUTs connected to the system.
 /// </summary>
-public interface IResourceDiscovery : OpenTap.ITapPlugin
+public interface IAssetDiscovery : OpenTap.ITapPlugin
 {
     /// <summary>
     /// Sets the priority of this provider. In case two implementations return the same 
-    /// discovered resource (same Identifier), the one from the higher priority provider is used
+    /// discovered asset (same Identifier), the one from the higher priority provider is used
     /// </summary>
     double Priority { get; }
-    IEnumerable<DiscoveredResource> DiscoverResources();
+    IEnumerable<DiscoveredAsset> DiscoverAssets();
 }
 
 /// <summary>
 /// Represents a discovered resource.
 /// IResourceDiscovery implementations can specialize this class to provide additional information.
 /// </summary>
-public class DiscoveredResource
+public class DiscoveredAsset
 {
     /// <summary>
-    /// The type of the resource. E.g. "N9020A".
-    /// This can be used to determine a suitable driver for the resource.
+    /// The type of the asset. E.g. "N9020A".
+    /// This can be used to determine a suitable driver for the asset
+    /// so it can be used as a Resource in OpenTAP.
     /// </summary>
     public string Model { get; set; }
     /// <summary>
-    /// A unique identifier for the resource. This is used to identify the resource in the system.
-    /// E.g. for an Instrument, this could be the serial number.
+    /// A unique identifier for the asset. This is used to identify the resource in the system.
+    /// E.g. for an Instrument, this could be a combination of Manufacturer, Model and serial number.
     /// </summary>
     public string Identifier { get; set; }
     /// <summary>
-    /// The firmware version of the resource.
+    /// The manufacturer of the asset. E.g. "Keysight". 
     /// </summary>
-    public string FirmwareVersion { get; set; }
+    public string Manufacturer { get; set; }
 
 }
 
--- OpenTap.Metrics/ResourceDiscovery/ResourceDiscoveryManager.cs
+++ OpenTap.Metrics/ResourceDiscovery/ResourceDiscoveryManager.cs
@@ -1,40 +1,49 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace OpenTap.Metrics.ResourceDiscovery;
+namespace OpenTap.Metrics.AssetDiscovery;
 
-public static class ResourceDiscoveryManager
+public static class AssetDiscoveryManager
 {
+    public static TraceSource log = OpenTap.Log.CreateSource("AssetDiscovery");
     /// <summary>
-    /// Returns all discovered resources from all available providers.
+    /// Returns all discovered assets from all available providers.
     /// </summary>
-    public static IEnumerable<DiscoveredResource> DiscoverAllResources()
+    public static IEnumerable<DiscoveredAsset> DiscoverAllAssets()
     {
-        Dictionary<string, DiscoveredResource> resources = new Dictionary<string, DiscoveredResource>();
-        foreach (var provider in GetResourceDiscoveryProviders())
+        Dictionary<string, DiscoveredAsset> assets = new Dictionary<string, DiscoveredAsset>();
+        foreach (var provider in GetAssetDiscoveryProviders())
         {
-            foreach (var res in provider.DiscoverResources())
+            try
             {
-                resources[res.Identifier] = res;
+                foreach (var asset in provider.DiscoverAssets())
+                {
+                    assets[asset.Identifier] = asset;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Error while discovering resources from {provider.GetType().Name}: {ex.Message}");
             }
         }
-        return resources.Values;
+        return assets.Values;
     }
 
-    private static IEnumerable<IResourceDiscovery> _resourceDiscoveryProviders;
-    private static IEnumerable<IResourceDiscovery> GetResourceDiscoveryProviders()
+    private static IEnumerable<IAssetDiscovery> _assetDiscoveryProviders;
+    private static IEnumerable<IAssetDiscovery> GetAssetDiscoveryProviders()
     {
-        if (_resourceDiscoveryProviders == null)
+        if (_assetDiscoveryProviders == null)
         {
-            _resourceDiscoveryProviders = TypeData.GetDerivedTypes<IResourceDiscovery>().Where(x => x.CanCreateInstance)
-                .Select(x => x.CreateInstance() as IResourceDiscovery)
+            _assetDiscoveryProviders = TypeData.GetDerivedTypes<IAssetDiscovery>().Where(x => x.CanCreateInstance)
+                .Select(x => x.CreateInstance() as IAssetDiscovery)
                 .OrderBy(x => x.Priority)
                 .ToList();
         }
-        return _resourceDiscoveryProviders;
+        return _assetDiscoveryProviders;
     }
 
-    // public static void PushDiscoveredResource(DiscoveredResource res)
+    // public static void PushDiscoveredAssets(DiscoveredAsset asset)
     // {
     //
     // }
