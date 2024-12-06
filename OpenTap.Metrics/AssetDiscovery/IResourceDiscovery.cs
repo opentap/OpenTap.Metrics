e8c07132d8ce3b42d1c3137ef05d55018c3aada0
Asger <20689829+AsgerIversen@users.noreply.github.com>
2024-12-06 12:40:54 +0100

    Update OpenTap.Metrics/AssetDiscovery/IResourceDiscovery.cs
    Co-authored-by: Heine Thranekjær Otte <nox@nox.dk>

--- OpenTap.Metrics/AssetDiscovery/IResourceDiscovery.cs
+++ OpenTap.Metrics/AssetDiscovery/IResourceDiscovery.cs
@@ -21,19 +21,19 @@ public interface IAssetDiscovery : OpenTap.ITapPlugin
 }
 
 /// <summary>
-/// Represents a discovered resource.
-/// IResourceDiscovery implementations can specialize this class to provide additional information.
+/// Represents a discovered asset.
+/// IAssetDiscovery implementations can specialize this class to provide additional information.
 /// </summary>
 public class DiscoveredAsset
 {
     /// <summary>
     /// The type of the asset. E.g. "N9020A".
     /// This can be used to determine a suitable driver for the asset
-    /// so it can be used as a Resource in OpenTAP.
+    /// so it can be used as an Asset in OpenTAP.
     /// </summary>
     public string Model { get; set; }
     /// <summary>
-    /// A unique identifier for the asset. This is used to identify the resource in the system.
+    /// A unique identifier for the asset. This is used to identify the asset in the system.
     /// E.g. for an Instrument, this could be a combination of Manufacturer, Model and serial number.
     /// </summary>
     public string Identifier { get; set; }
