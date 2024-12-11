11a834ca9213bcc57ea6ffb94ae89290a6ea33c2
Asger <20689829+AsgerIversen@users.noreply.github.com>
2024-12-11 16:04:18 +0100

    Added IAssetDiscovery plugin type an IOLibs implementation that uses KCCS service (#45)
    * Also publish alphas (so we can test with Runner)
    
    * Bumped version since we have new APIs
    
    * Only return distinct resource identifiers
    
    * Refactor to get rid of websocket.client nuget that requires netstandard2.1
    
    * Temporarily target net6.0 to make this work just for testing. We should split the Kccs impl out to and revert back to netstandard later
    
    * Moved KccsResourceDiscovery implementation to new repo at github.com/opentap/OpenTap.Kccs
    
    * Fixed issue where IResourceDiscovery new implementations would get instanciated on every poll
    
    * Renamed ResourceDiscovery to AssetDiscovery
    
    * Renamed folder
    
    * Minor cleanup
    
    * Update OpenTap.Metrics/AssetDiscovery/ResourceDiscoveryManager.cs
    
    Co-authored-by: Heine Thranekjær Otte <nox@nox.dk>
    
    * Update OpenTap.Metrics/AssetDiscovery/IResourceDiscovery.cs
    
    Co-authored-by: Heine Thranekjær Otte <nox@nox.dk>
    
    * Renamed files
    
    * Redesigned interfase to allow IAssetDiscovery implementations to respond with "IsBusy"
    
    * Renamed IsBusy to IsValid to be able to use it for errors as well
    
    * Added Error string to DiscoveryResult
    
    * Return DiscoveryResult with IsSuccess=false on exceptions
    
    * Update OpenTap.Metrics/AssetDiscovery/AssetDiscoveryManager.cs
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>
    
    * Update .gitversion
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>
    
    * Update OpenTap.Metrics/AssetDiscovery/AssetDiscoveryManager.cs
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>
    
    ---------
    
    Co-authored-by: Heine Thranekjær Otte <nox@nox.dk>
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>

---
+++ OpenTap.Metrics/AssetDiscovery/AssetDiscoveryManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenTap.Metrics.AssetDiscovery;
+
+public static class AssetDiscoveryManager
+{
+    private static TraceSource log = OpenTap.Log.CreateSource("AssetDiscovery");
+
+    /// <summary>
+    /// Returns all discovered assets from all available providers.
+    /// </summary>
+    public static Dictionary<IAssetDiscovery, DiscoveryResult> DiscoverAllAssets()
+    {
+        Dictionary<IAssetDiscovery, DiscoveryResult> assets = new Dictionary<IAssetDiscovery, DiscoveryResult>();
+        foreach (var provider in GetAssetDiscoveryProviders())
+        {
+            try
+            {
+                var result = provider.DiscoverAssets();
+                assets[provider] = result;
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Error while discovering assets from {provider.GetType().Name}: {ex.Message}");
+                assets[provider] = new DiscoveryResult
+                {
+                    IsSuccess = false,
+                    Error = ex.Message
+                };
+            }
+        }
+        return assets;
+    }
+
+    private static IEnumerable<IAssetDiscovery> _assetDiscoveryProviders;
+    private static IEnumerable<IAssetDiscovery> GetAssetDiscoveryProviders()
+    {
+        if (_assetDiscoveryProviders == null)
+        {
+            _assetDiscoveryProviders = TypeData.GetDerivedTypes<IAssetDiscovery>().Where(x => x.CanCreateInstance)
+                .Select(x => x.CreateInstance() as IAssetDiscovery)
+                .OrderByDescending(x => x.Priority)  // Higher (numeric value) priority should be used first.
+                .ToList();
+        }
+        return _assetDiscoveryProviders;
+    }
+
+    // public static void PushDiscoveredAssets(DiscoveredAsset asset)
+    // {
+    //
+    // }
+}
---
+++ OpenTap.Metrics/AssetDiscovery/IAssetDiscovery.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+
+namespace OpenTap.Metrics.AssetDiscovery;
+
+/// <summary>
+/// Plugin interface for asset discovery.
+/// Implementations of this interface can be used to discover assets such as instruments or DUTs connected to the system.
+/// </summary>
+public interface IAssetDiscovery : OpenTap.ITapPlugin
+{
+    /// <summary>
+    /// Sets the priority of this provider. In case two implementations return the same 
+    /// discovered asset (same Identifier), the one from the higher priority provider is used
+    /// </summary>
+    double Priority { get; }
+
+    /// <summary>
+    /// Discovers assets connected to the system.
+    /// </summary>
+    DiscoveryResult DiscoverAssets();
+}
+
+public class DiscoveryResult
+{
+    /// <summary>
+    /// If false, the discovery cannot be performed because of an error or because the system is busy. 
+    /// E.g. a test is running that might be impacted by sending a *IDN? SCPI query.
+    /// An empty list in this case does not mean that no assets are available.
+    /// </summary>  
+    public bool IsSuccess { get; set; }
+
+    /// <summary>
+    /// If IsSuccess is false, this property should contain a short message as to why the discovery failed.
+    /// </summary>
+    public string Error { get; set; }
+    /// <summary>
+    /// List of assets discovered by a specific implementation of IAssetDiscovery.
+    /// Null or an empty list only means no assets were found if IsSuccess is true.
+    /// </summary>
+    public IEnumerable<DiscoveredAsset> Assets { get; set; }
+}
+
+
+/// <summary>
+/// Represents a discovered asset.
+/// IAssetDiscovery implementations can specialize this class to provide additional information.
+/// </summary>
+public class DiscoveredAsset
+{
+    /// <summary>
+    /// The type of the asset. E.g. "N9020A".
+    /// This can be used to determine a suitable driver for the asset
+    /// so it can be used as an Asset in OpenTAP.
+    /// </summary>
+    public string Model { get; set; }
+    /// <summary>
+    /// A unique identifier for the asset. This is used to identify the asset in the system.
+    /// E.g. for an Instrument, this could be a combination of Manufacturer, Model and serial number.
+    /// </summary>
+    public string Identifier { get; set; }
+    /// <summary>
+    /// The manufacturer of the asset. E.g. "Keysight". 
+    /// </summary>
+    public string Manufacturer { get; set; }
+}
+
+
